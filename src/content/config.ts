7fcdf3ca4f48cf68a779cfda3bf5ec2ccb267b46
Abhigyan Tripathi <abhigyantrips@gmail.com>
2024-08-21 08:56:39 +0530

    chore(dx): run prettier

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,10 +1,12 @@
-import { defineCollection, z } from "astro:content";
+import { defineCollection, z } from 'astro:content';
 
 export const collections = {
 	posts: defineCollection({
-		schema: ({ image }) => z.object({
-			images: z.array(image()),
-			caption: z.string(),
-		}),
-	})
-}
+		type: 'data',
+		schema: ({ image }) =>
+			z.object({
+				images: z.array(image()),
+				caption: z.string(),
+			}),
+	}),
+};
